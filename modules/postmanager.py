dd7f0cd090e0633bf116a3f19e9f4320727f4da7
Lacilo <144881417+Lacilo@users.noreply.github.com>
2024-12-21 21:16:20 +0100

    alprogramok létrehozása
    A postmanager.py modult megtöltöttem azokkal az alprogramokkal is amikről úgy gondoltam közük lehet a fájlkezeléshez

---
+++ modules/postmanager.py
@@ -0,0 +1,14 @@
+def delete(): # Meghívja a fájlkezelés | törlési funkcióját |
+    pass
+
+def edit(): # Meghívja a fájlkezelés | szerkesztési funkcióját |
+    pass
+
+def create(): # Megívja a fájlkezelés | létrehozás funkcióját |
+    pass
+
+def list(): # Bejegyzés | címek listázása |
+    pass
+
+def open(): # Kért | bejegyzés megnyitása |
+    pass
