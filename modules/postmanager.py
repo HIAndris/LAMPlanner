66d2be81c8f5b84ef33d213fd050a5e58a20e2f2
Lacilo <144881417+Lacilo@users.noreply.github.com>
2024-12-21 21:34:59 +0100

    Update postmanager.py

--- modules/postmanager.py
+++ modules/postmanager.py
@@ -1,14 +1,14 @@
-def delete(): # Meghívja a fájlkezelés | törlési funkcióját |
+def Delete(): # Meghívja a fájlkezelés | törlési funkcióját |
     pass
 
-def edit(): # Meghívja a fájlkezelés | szerkesztési funkcióját |
+def Edit(): # Meghívja a fájlkezelés | szerkesztési funkcióját |
     pass
 
-def create(): # Megívja a fájlkezelés | létrehozás funkcióját |
+def Create(): # Megívja a fájlkezelés | létrehozás funkcióját |
     pass
 
-def list(): # Bejegyzés | címek listázása |
+def List(): # Bejegyzés | címek listázása |
     pass
 
-def open(): # Kért | bejegyzés megnyitása |
+def Open(): # Kért | bejegyzés megnyitása |
     pass
