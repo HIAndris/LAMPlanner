ad752660f2565a85055f11d3f07c2659c28eade0
Hegedus Istvan Andras <hegedusi@kkszki.hu>
2024-12-24 16:07:50 +0100

    Felhasználó létrehozása

--- lamplanner.py
+++
@@ -1,3 +0,0 @@
-from modules import usermanager as um
-from modules import postmanager as pm
-from modules import filemanager as fm
--- modules/filemanager.py
+++ modules/filemanager.py
@@ -8,27 +8,75 @@ def WorkingDir():
         return os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
 
 
-def SignUp(name, password) -> bool:
+def SignUp(name: str, password: str) -> bool:
+    """Adds a user to the user database and creates a dictionary for them
+
+    Args:
+        name (str): the chosen username
+        password (str): the chosen password
+
+    Returns:
+        bool: Returns True if the user has been created and returns False and a description if there was a problem during the creation.
+    """
+    import os
+    
     inDir = WorkingDir()
     
+    if name in Users():
+        return False, "This username already exists!"
     
+    if "\n" in name:
+        return False, "The name has newline character(s) in it!"
+    
+    passwordHash = CreatePasswordHash(password)
+    users = Users("s")
+    if len(users) == 0:
+        serial = 0
+    else:
+        serial = max(users) + 1
+    
+    with open(os.path.join(inDir, "users", "users.txt"), "a", encoding="utf-8") as f:
+        f.write(name + " " + str(serial) + " " + passwordHash + "\n")
+    
+    os.makedirs(os.path.dirname(inDir, "users", str(serial)), exist_ok = True)
+    with open(os.path.join(inDir, "users", str(serial), "user.hiasecret"), "", encoding="utf-8") as f:
+        pass
+    
+    return True
 
 
-def LogIn(name, password) -> bool:
+def LogIn(name: str, password: str) -> bool:
     pass
 
 
-def CreatePasswordHash(password: str) -> bytes:
+def CreatePasswordHash(password: str) -> str:
+    """Creates the password hash used to validate a password.
+
+    Args:
+        password (str): user's password
+
+    Returns:
+        str: the password hash in str
+    """
     import hashlib
     import base64
 
     hash = hashlib.sha3_512(password.encode())
     base64Hash = base64.b64encode(hash.digest())
     
-    return base64Hash
+    return base64Hash.decode("utf-8")
 
 
 def CreateEncryptionKey(name: str, password: str) -> bytes:
+    """Creates the encryption key used for storing data.
+
+    Args:
+        name (str): username
+        password (str): the user's password
+
+    Returns:
+        bytes: the encryption key
+    """
     import hashlib
     
     hash = hashlib.sha256(password.encode()).digest()
@@ -89,11 +137,11 @@ def Decrypt(data: bytes, key: bytes) -> str:
     return decrypted.decode("utf-8")
 
 
-def Store(name: str, key: bytes, title: str, text: str, date: str = None, status: bool = False) -> bool:
+def Store(id: str, key: bytes, title: str, text: str, date: str = None, status: bool = False) -> bool:
     pass
 
 
-def Read(name: str, key: bytes, title: str) -> list:
+def Read(id: str, key: bytes, title: str) -> list:
     pass
 
 
@@ -101,7 +149,7 @@ def Users(mode: str = "u") -> list:
     """Returns the users stored in the program.
 
     Args:
-        mode (str, optional): The mode sets the type of information returned back: "u"= usernames, "f"= usernames and serial numbers, "s"= serial numbers. Defaults to "u".
+        mode (str, optional): The mode sets the type of information returned back: "u"= usernames, "f"= usernames, serial numbers and password hashes, "s"= serial numbers, "v"=usernames and password hashes. Defaults to "u".
 
     Raises:
         SyntaxError: Invalid mode is given.
@@ -112,7 +160,7 @@ def Users(mode: str = "u") -> list:
     """
     import os
     
-    if mode not in ("u", "f", "s"):
+    if mode not in ("u", "f", "s", "v"):
         raise SyntaxError("Invalid mode for filemanager.Users()!")
     users = []
     inDir = WorkingDir()
@@ -126,24 +174,25 @@ def Users(mode: str = "u") -> list:
                 
             else:
                 try:
-                    name, serial = line.strip().split(" ")
+                    name, serial, passwordHash = line.strip().rsplit(" ", 2)
                 except Exception as e:
                     raise SyntaxError("The users.txt was modified and one of the lines has incorrect syntax!")
 
                 if mode == "u": users.append(name)
-                elif mode == "f": users.append((name, serial))
-                else: users.append(serial)
+                elif mode == "f": users.append((name, int(serial), passwordHash))
+                elif mode == "s": users.append(int(serial))
+                else: users.append((name, passwordHash))
                 
     return users
 
 
-def EditProperties(name: str, key: bytes, title: str, newTitle: str = None, newDate: str = None, newStatus: bool = None):
+def EditProperties(id: str, key: bytes, title: str, newTitle: str = None, newDate: str = None, newStatus: bool = None):
     pass
 
 
-def UserStored(name: str, key: bytes) -> list:
+def UserStored(id: str, key: bytes) -> list:
     pass
 
 
-def Delete(name: str, key: bytes, title: str) -> bool:
+def Delete(id: str, key: bytes, title: str) -> bool:
     pass
