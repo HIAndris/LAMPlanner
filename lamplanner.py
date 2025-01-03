98851e8f37584951294ec66a9131076b2ce6034e
Lacilo <szaszfail@kkszki.hu>
2025-01-04 00:01:43 +0100

    Bejelentkezés, felhasználó törlés

--- lamplanner.py
+++ lamplanner.py
@@ -1,15 +1,36 @@
 from modules import postmanager
 from modules import usermanager
 from modules import filemanager
+import os
 
 running = True
 loggedIn = False
-name = None
+username = ""
+
 while running:
-    while not loggedIn:
-        #postmanager.GetUnameGetHash()
-        pass
+    if not loggedIn:
+        os.system("cls")
+        choice = input("Nincs bejelentkezve --> ")
+
+        if choice == "login":
+            os.system("cls")
+            username, password, loggedIn = usermanager.LogIn()
+        if choice == "delete":
+            os.system("cls")
+            usermanager.Delete()
+        if choice == "exit":
+            break
     
-    while loggedIn:
-        #postmanager.MainLoop()
-        pass
+    if loggedIn:
+        postmanager.GetUnameGetHash(username, password)
+
+        user_input = input(f"\n{username} --> ")
+
+        if user_input != "logout":
+            user_input = user_input.split(" ", 1)
+            user_input[0] = user_input[0].capitalize()
+            user_input.append("")
+
+            postmanager.MainLoop(user_input)
+        else:
+            loggedIn = False
--- modules/usermanager.py
+++ modules/usermanager.py
@@ -1,13 +1,76 @@
 from modules import filemanager
+import os
+
+def ValidatePassword():
+    pass
 
 def LogIn():
-    pass
+    login = False
 
-def LogOut():
-    pass
+    users = filemanager.Users("u")
+
+    username = " "
+
+    while not login and username != "":
+        username = input("Felhasználónév (hagyja üresen a visszalépéshez) --> ")
+
+        if username in users:
+            password = input("----------------------------------------------\nJelszó --> ")
+
+            login = filemanager.LogIn(username, password)
+
+            if login:
+                os.system("cls")
+                print("Sikeres bejelentkezés, Üdvözöljük! Használja a help parancsot a parancssegédlet megjelenítéséhez\n") 
+
+                return username, password, True
+                # Mehet a postmanager
+            else:
+                os.system("cls")
+                print("Helytelen Jelszó, próbálja újra\n-------------------------------\n")
+
+        elif username not in users and username != "":
+            os.system("cls")
+            print("Nem létezik ilyen felhasználó!\n------------------------------\n")
+
+    return "", "", False
+
 
 def SignUp():
     pass
 
 def Delete():
-    pass
+    users = filemanager.Users("u")
+
+    username = input("Törölni kívánt fiók felhasználóneve (hagyja üresen a visszalépéshez) --> ")
+
+    if username in users:
+        password = input("--------------------------------------------------------------------\nTörölni kívánt fiók jelszava --> ")
+
+        os.system("cls")
+
+        if input(f"\nBiztosan törölni akaja a {username} nevű fiókot? (igen/nem) --> ") == "igen":
+            deleted = filemanager.DeleteUser(username, password)
+
+            if deleted:
+                os.system("cls")
+                print("Felhasználó sikeresen törölve")
+            else:
+                os.system("cls")
+                print("Helytelen Jelszó, próbálja újra\n-------------------------------\n")
+                Delete()
+        else:
+            os.system("cls")
+            print("Nem töröltünk fiókot.")
+
+    elif username == "":
+        os.system("cls")
+        print("Nem töröltünk felhasználót")
+
+    else:
+        os.system("cls")
+        print("Nem létezik ilyen felhasználó!\n------------------------------\n")
+        Delete()
+
+
+filemanager.SignUp("Jóska", "Jelszó")
